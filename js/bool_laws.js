14d3053ba9cc0edcc62c4091f7ab3a294078b5dd
JChris246 <chris.401@live.com>
2019-11-03 22:22:17 -0400

    Minor changes
    changed background image to ref local image instead of internet url
    adjusted for loop to foreach with callback

--- js/bool_laws.js
+++ js/bool_laws.js
@@ -3,8 +3,10 @@ const database = {"1var": [], "2var": [], "3var": [], "4var": []};
 function readParams() {
 	let checkBox = document.querySelectorAll("input[name]");
 	let options = [];
-	for(i = 0; i < checkBox.length; i++)
-		if (checkBox[i].checked)
-			options.push(checkBox[i].name);
+	
+	checkBox.forEach(function(b) {
+		 if (b.checked)
+			options.push(b.name)
+	});
 	alert("No further action coded as yet");
 }
