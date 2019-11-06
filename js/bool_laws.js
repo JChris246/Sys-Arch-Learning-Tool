3156378f00ecd02fba1138a3d69956b7622f5546
JChris246 <chris.401@live.com>
2019-11-06 00:07:15 -0400

    Alot of changes - where to start
    removed top bar all (all pages) with search topics, username + password and register
    removed duplicated boolean algebra panel
    slightly adjusted position of truth table in bool_laws.html
    added 10 questions too be randomly asked as practice questions
    	will tell you if your attempt is right
    	will give you the truth table of the equation
    	will give you correct answer if enter it wrong
    	tried best to ensure it doesnt matter the symbol you use (+, or, | - for or)
    Changed some appearance details of index.html

--- js/bool_laws.js
+++ js/bool_laws.js
@@ -1,35 +1,67 @@
-const database = {"1var": [{'q': 'a + ~a', 'a': '1'}, {'q': 'a.~a', 'a': '0'}],
-"2var": [],
-"3var": [],
-"4var": []
-};
+const database = [
+	{q: 'a + ~a', a: '1', t: [['a', 'output'], ['0', '1'], ['1', '1']]},
+	{q: 'a * ~a', a: '0', t: [['a', 'output'], ['0', '0'], ['1', '0']]},
+	{q: 'a * ~a + a', a: 'a', t: [['a', 'output'], ['0', '0'], ['1', '1']]},
+	{q: '~(a * b) * (~a + b) * (~b + b)', a: '~a + ~b * b', t: [['a', 'b', 'output'], ['0', '0', '1'], ['0', '1', '1'], ['1', '0', '0'], ['1', '1', '0']]},
+	{q: 'p + ~(p * q)', a: '1', t: [['p', 'q', 'output'], ['0', '0', '1'], ['0', '1', '1'], ['1', '0', '1'], ['1', '1', '1']]},
+	{q: 'p + ~(p + q)', a: 'p + ~q', t: [['p', 'q', 'output'], ['0', '0', '1'], ['0', '1', '0'], ['1', '0', '1'], ['1', '1', '1']]},
+	{q: '~(p + q) + ~q', a: '~q', t: [['p', 'q', 'output'], ['0', '0', '1'], ['0', '1', '0'], ['1', '0', '1'], ['1', '1', '0']]},
+	{q: 'ab~c + abc', a: 'ab', t: [['a', 'b', 'c', 'output'], ['0', '0', '0', '0'], ['0', '0', '1', '0'], ['0', '1', '0', '0'], ['0', '1', '1', '0'], ['1', '0', '0', '0'], ['1', '0', '1', '0'], ['1', '1', '0', '1'], ['1', '1', '1', '1']]},
+	{q: 'a~bc + abc', a: 'ac', t: [['a', 'b', 'c', 'output'], ['0', '0', '0', '0'], ['0', '0', '1', '0'], ['0', '1', '0', '0'], ['0', '1', '1', '0'], ['1', '0', '0', '0'], ['1', '0', '1', '1'], ['1', '1', '0', '0'], ['1', '1', '1', '1']]},
+	{q: '~a~bc+ ~ab~c + a~b~c + a~bc + abc', a: 'ac + a~b + b~a~c + c~b', t: [['a', 'b', 'c', 'output'], ['0', '0', '0', '0'], ['0', '0', '1', '1'], ['0', '1', '0', '1'], ['0', '1', '1', '0'], ['1', '0', '0', '1'], ['1', '0', '1', '1'], ['1', '1', '0', '0'], ['1', '1', '1', '1']]}];
 
-//compare with case insensitivity, also replace all and (.,*,&) symbols to null
-const synonyms = {'t': 'true,t,1', 'f': 'false,f,0', '~': '~,!', '+': 'v,or,+,|'};
+var complete = []; //keep track of questions asked already
+var count = 0;
 
-var options = [], count = 0;
-
-function readParams() {
-	let checkBox = document.querySelectorAll("input[name]");
-	
-	checkBox.forEach(function(b) {
-		 if (b.checked)
-			options.push(b.name)
-	});
-	createQuestion();
+function init() {
+	for(i = 0; i < database.length; i++)
+		complete.push(false);
 }
 
+function createPad() {
+	//if previous pad existed, remove it
+	let temp = document.querySelector("div.center");
+	if (temp)
+		temp.remove();
 
-function clearPad() {
-	let temp = document.querySelectorAll("div.center")[0]; //center to remove
-	let header = temp.querySelector("span.header").cloneNode();
-	let center = temp.cloneNode(); //clone without child nodes
-	document.getElementById("main").appendChild(center); //replace with cloned
-	temp.remove();
-	return header;
+	let header = document.createElement("span");
+	header.classList.add("header");
+	header.classList.add("greenbg");
+	header.innerHTML = "Simplify the following boolean expression";
+
+	let div = document.createElement("div");
+	div.classList.add("center");
+	div.appendChild(header);
+
+	document.getElementById("main").appendChild(div);
+	return div;
 }
 
-function createEnterBtn(attempt, ans) {
+function convert(raw) {
+	raw = raw.replace(new RegExp("[tT][Rr][Uu][eE]|[Tt]", 'g'), "1");
+	raw = raw.replace(new RegExp("[Ff][Aa][Ll][Ss][Ee]|[Ff]", 'g'), "0");
+	raw = raw.replace(new RegExp("[!]", 'g'), "~");
+	raw = raw.replace(new RegExp("v|or|[|]", 'g'), "[+]");
+	raw = raw.replace(new RegExp("[.]|[*][&]", 'g'), "");
+	raw = raw.replace(new RegExp("\s+", 'g'), "");
+	return raw.toLowerCase();
+}
+
+function createTable(values) {
+	let table = document.createElement("table");
+	for (i = 0; i < values.length; i++) {
+		let tempTR = document.createElement("tr");
+		for(j = 0; j < values[i].length; j++) {
+			let tempCol = document.createElement(i == 0 ? "th": "td");
+			tempCol.innerHTML = values[i][j];
+			tempTR.appendChild(tempCol);
+		}
+		table.appendChild(tempTR);
+	}
+	return table;
+}
+
+function createEnterBtn(attempt, ans, table) {
 	let btn = document.createElement("button");
 	btn.classList.add("submit");
 	btn.classList.add("greenbg");
@@ -42,66 +74,91 @@ function createEnterBtn(attempt, ans) {
 
 		give button to advance to next question*/
 
-		let header = clearPad();
-		if (attempt.value == ans) { //at this point to do subs with synonyms
-			header.innerHTML = "Your answer is correct";
+		if (attempt.value.length == 0) {
+			alert("You should probably enter a value as an attempt");
 		} else {
-			header.innerHTML = "Your answer is incorrect";
-			header.classList.remove("greenbg");
-			header.classList.add("redbg");
+			let header = createPad().querySelector("span.header");
+			//translate user symbols (if not used the same as ans)
+			let cleanAttempt = convert(attempt.value);
+
+			//make a container to put next button, correct ans (if wrong) and truth table in
+			container = document.createElement("div");
+			container.classList.add("result");
+			if (table)
+				container.appendChild(createTable(table));
+
+			if (cleanAttempt == ans) {
+				header.innerHTML = "Your answer is correct";
+			} else {
+				header.innerHTML = "Your answer is incorrect";
+				header.classList.remove("greenbg");
+				header.classList.add("redbg");
+
+				//give correct ans
+				span = document.createElement("span");
+				span.classList.add("correction");
+				span.classList.add("blue");
+				span.innerHTML = "The correct answer is "+ans;
+
+				container.appendChild(span);
+			}
+
+			//setup a button to go to next question
+			next = document.createElement("button");
+			next.classList.add("submit");
+			next.classList.add("greenbg");
+			next.innerHTML = "Next";
+			next.onclick = () => createQuestion();
+			container.appendChild(next);
+
+			//add header (correct or incorrect) and container with next to main container
+			document.querySelectorAll("div.center")[0].appendChild(header);
+			document.querySelectorAll("div.center")[0].appendChild(container);
 		}
-
-		//this would be a good place to print truthtable
-
-		//setup a button to go to next question
-		next = document.createElement("button");
-		next.classList.add("submit");
-		next.classList.add("greenbg");
-		next.innerHTML = "Next";
-		next.onclick = function() {
-			header.classList.remove("redbg");
-			header.classList.add("greenbg")
-			createQuestion();
-		}
-
-		//make a container to put next button in
-		container = document.createElement("div");
-		container.classList.add("result");
-		container.appendChild(next);
-
-		//add header (correct or incorrect) and container with next to main container
-		document.querySelectorAll("div.center")[0].appendChild(header);
-		document.querySelectorAll("div.center")[0].appendChild(container);
-
 	};
 
 	btn.innerHTML = "Enter";
 	return btn;
 }
 
+function getRandomIndex() {
+	let valid = false, j = 0;
+
+	while(!valid) { //loop till we have a index with a question not previously asked
+		j = Math.floor(Math.random() * database.length); // 0 - database length
+		valid = !complete[j]; //avoid using a previously question
+	}
+	return j;
+}
+
 function createQuestion() {
-	let header = clearPad();
-	let center = document.querySelectorAll("div.center")[0];
-	header.innerHTML = "Simplify the following boolean expression";
-	center.appendChild(header);
+	//if u have asked database.length times, this means u are out of questions
+	//(because questions are only asked once although random)
+	if (count >= database.length) {
+		alert("We have no more questions to ask");
+		//probably display a total of correct ans screen here
+	} else {
+		let center = createPad();
 
-	let ansBox = document.createElement("div");
-	let ans = document.createElement("input");
-	ans.setAttribute('type', 'text');
-	ans.setAttribute('value', '');
+		let ansBox = document.createElement("div");
+		let ans = document.createElement("input");
+		ans.setAttribute('type', 'text');
+		ans.setAttribute('value', '');
 
-	let btn = createEnterBtn(ans, database['1var'][count]['a']);
+		let i = getRandomIndex();
+		let btn = createEnterBtn(ans, database[i].a, database[i].t);
 
-	let question = document.createElement("p");
-	question.classList.add("description");
-	//again this is hardcoded, will choose based on user interaction later (if to do 1var/2var or combo)
-	question.innerHTML = database['1var'][count]['q'];
-	count = ++count % database['1var'].length; //prevent overflow
+		let question = document.createElement("p");
+		question.classList.add("description");
 
-	ansBox.appendChild(ans);
-	ansBox.appendChild(document.createElement("p"));
-	ansBox.appendChild(btn);
+		question.innerHTML = database[i].q;
+		count++;
 
-	center.appendChild(question);
-	center.appendChild(ansBox);
+		ansBox.appendChild(ans);
+		ansBox.appendChild(document.createElement("p"));
+		ansBox.appendChild(btn);
+
+		center.appendChild(question);
+		center.appendChild(ansBox);
+	}
 }
